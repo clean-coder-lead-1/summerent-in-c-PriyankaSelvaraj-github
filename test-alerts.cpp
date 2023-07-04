e441f40cac88ab7990b749a85c83e1d4faf365e6
PriyankaSelvaraj-github <138089914+PriyankaSelvaraj-github@users.noreply.github.com>
2023-07-04 21:53:40 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -14,3 +14,21 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(50, 20, 30) == TOO_HIGH);
 }
+
+TEST_CASE("infers the breach according to cooling type") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING , 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING , 35) == NORMAL);
+
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING , 36) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING , 36) == NORMAL);
+
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 41) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING , 41) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING , 41) == NORMAL);
+  
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 46) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING , 46) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING , 46) == TOO_HIGH);
+}
